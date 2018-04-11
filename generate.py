d2890f6745916f4b1eeec8d623fa3e4d9f9e9b8f
BlissChapman <bliss.chapman@gmail.com>
2018-04-10 19:34:05 -0500

    Added real datasets to the gitignore.

--- generate.py
+++ generate.py
@@ -1,67 +1,22 @@
 import argparse
-import datetime
-import models.ProbabilityDistGAN
-import numpy as np
+import numpy
 import os
 import shutil
-import torch
 
-from torch.autograd import Variable
-from utils.sampling import uniform_noise
+from utils.sampling import *
 
-parser = argparse.ArgumentParser(description="Generate specified number of samples from trained generator and write to specified output directory.")
-parser.add_argument('generator_state_dict_path', help='path to a file containing the generative model state dict')
+# Parse arguments
+parser = argparse.ArgumentParser(description="Generate specified number of samples from hardcoded distribution and write to specified output directory.")
 parser.add_argument('num_samples', type=int, help='the number of samples to generate')
 parser.add_argument('output_dir', help='the directory to save generated samples')
 args = parser.parse_args()
 
-# ========== HOUSEKEEPING ==========
-CUDA = torch.cuda.is_available()
-if CUDA:
-    print("Using GPU optimizations!")
-
-np.random.seed(1)
-torch.manual_seed(1)
-if CUDA:
-    torch.cuda.manual_seed(1)
-
+# Housekeeping
 shutil.rmtree(args.output_dir, ignore_errors=True)
 os.makedirs(args.output_dir)
+np.random.seed(1)
 
-# ========== Hyperparameters ==========
-BATCH_SIZE = 16
-MODEL_DIMENSIONALITY = 64
-SAMPLE_LENGTH = 1
-NOISE_SAMPLE_LENGTH = 64
-
-description_f = open(args.output_dir + 'description.txt', 'w')
-description_f.write('DATE: {0}\n\n'.format(datetime.datetime.now().strftime('%b-%d-%I%M%p-%G')))
-description_f.write('BATCH_SIZE: {0}\n'.format(BATCH_SIZE))
-description_f.write('MODEL_DIMENSIONALITY: {0}\n'.format(MODEL_DIMENSIONALITY))
-description_f.write('SAMPLE_LENGTH: {0}\n'.format(SAMPLE_LENGTH))
-description_f.write('NOISE_SAMPLE_LENGTH: {0}\n'.format(NOISE_SAMPLE_LENGTH))
-description_f.close()
-
-# ========== Models ==========
-generator = models.ProbabilityDistGAN.Generator(input_width=NOISE_SAMPLE_LENGTH,
-                                                output_width=SAMPLE_LENGTH,
-                                                dimensionality=MODEL_DIMENSIONALITY,
-                                                cudaEnabled=CUDA)
-generator.load_state_dict(torch.load(args.generator_state_dict_path))
-
-# ========== Sample Generation ==========
-synthetic_data = []
-for step in range(args.num_samples):
-    # Generate synthetic data
-    noise_sample = Variable(uniform_noise(NOISE_SAMPLE_LENGTH, BATCH_SIZE, CUDA))
-    synthetic_data_sample = generator(noise_sample).data.numpy().flatten()
-    synthetic_data_sample = list(synthetic_data_sample)
-    synthetic_data += synthetic_data_sample
-
-    # Logging
-    print("PERCENT GENERATED: {0:.2f}%\r".format(100.0 * float(step) / float(args.num_samples)), end='')
-
-# Write samples to disk
-synthetic_data = np.array(synthetic_data)
-np.save(args.output_dir + 'synthetic_data', synthetic_data)
-print("Synthetic data saved to '{0}'".format(args.output_dir + 'synthetic_data'))
+# Generate samples and save them to disk
+data = gaussian(args.num_samples)
+np.save(args.output_dir + 'data', data)
+print("Data saved to '{0}'".format(args.output_dir + 'data'))
