- Switched to bootstrap calculation of ground truth regions of interest. - Adjusted to weighted tp, tn, fp, fn. - Fixed bug in calculating overall brain rejection. - Added plot of percent brain voxels rejecting.

fmri_power_analysis_comparison.py
@@ -7,7 +7,7 @@ import shutil
 
 from brainpedia.brainpedia import Brainpedia
 from brainpedia.fmri_processing import invert_preprocessor_scaling
-from utils.multiple_comparison import rejecting_voxels, power_calculations
+from utils.multiple_comparison import bootstrap_rejecting_voxels_mask, fmri_power_calculations
 from nilearn import plotting
 from utils.sampling import *
 
@@ -76,31 +76,34 @@ real_visual_brain_img = invert_preprocessor_scaling(real_dataset_visual[0].squee
 syn_non_visual_brain_img = invert_preprocessor_scaling(syn_dataset_non_visual[2].squeeze(), syn_dataset_brainpedia.preprocessor)
 syn_visual_brain_img = invert_preprocessor_scaling(syn_dataset_visual[0].squeeze(), syn_dataset_brainpedia.preprocessor)
 
-figure, axes = plt.subplots(nrows=9, ncols=1, figsize=(15, 40))
+figure, axes = plt.subplots(nrows=10, ncols=1, figsize=(15, 40))
 plotting.plot_glass_brain(real_non_visual_brain_img, threshold='auto', title="[REAL NON-VISUAL]", axes=axes[0])
 plotting.plot_glass_brain(syn_non_visual_brain_img, threshold='auto', title="[SYN NON-VISUAL]", axes=axes[1])
 plotting.plot_glass_brain(real_visual_brain_img, threshold='auto', title="[REAL VISUAL]", axes=axes[2])
 plotting.plot_glass_brain(syn_visual_brain_img, threshold='auto', title="[SYN VISUAL]", axes=axes[3])
 
-# Compute voxels that are statistically significant indicators of non-visual vs visual
-visual_vs_non_visual_rejecting_voxels = rejecting_voxels(real_dataset_visual.squeeze(), real_dataset_non_visual.squeeze())
+# Compute statistical significance weights of each voxel in non-visual vs visual
+k = 10
+visual_vs_non_visual_rejecting_voxels = bootstrap_rejecting_voxels_mask(real_dataset_visual.squeeze(), real_dataset_non_visual.squeeze(), k=k)
 
 # Compute power for various n
-k = 10*1
 n = np.geomspace(2, syn_dataset_length, num=25)
 
 fdr_test_power_for_n = np.zeros(len(n))
 syn_fdr_test_power_for_n = np.zeros(len(n))
 
-tp_ratios_syn_for_n = np.zeros((len(n), k))
-tn_ratios_syn_for_n = np.zeros((len(n), k))
-fp_ratios_syn_for_n = np.zeros((len(n), k))
-fn_ratios_syn_for_n = np.zeros((len(n), k))
+percent_rejecting_voxels_syn_for_n = np.zeros((len(n), k))
+percent_rejecting_voxels_real_for_n = np.zeros((len(n), k))
 
-tp_ratios_real_for_n = np.zeros((len(n), k))
-tn_ratios_real_for_n = np.zeros((len(n), k))
-fp_ratios_real_for_n = np.zeros((len(n), k))
-fn_ratios_real_for_n = np.zeros((len(n), k))
+wtp_syn_for_n = np.zeros((len(n), k))
+wtn_syn_for_n = np.zeros((len(n), k))
+wfp_syn_for_n = np.zeros((len(n), k))
+wfn_syn_for_n = np.zeros((len(n), k))
+
+wtp_real_for_n = np.zeros((len(n), k))
+wtn_real_for_n = np.zeros((len(n), k))
+wfp_real_for_n = np.zeros((len(n), k))
+wfn_real_for_n = np.zeros((len(n), k))
 
 for i in range(len(n)):
     # Determine sample sizes to draw from synthetic and real datasets
@@ -108,69 +111,76 @@ for i in range(len(n)):
     #   max available amount of data.
     syn_n = int(n[i])
     real_n = min(real_dataset_non_visual.shape[0], int(n[i]))
-    fdr_syn_power, tp_ratios_syn, tn_ratios_syn, fp_ratios_syn, fn_ratios_syn = power_calculations(syn_dataset_non_visual, syn_dataset_visual, syn_n, syn_n, visual_vs_non_visual_rejecting_voxels, k=k)
-    fdr_real_power, tp_ratios_real, tn_ratios_real, fp_ratios_real, fn_ratios_real = power_calculations(real_dataset_non_visual, real_dataset_visual, real_n, real_n, visual_vs_non_visual_rejecting_voxels, k=k)
+    fdr_syn_power, percent_rejecting_voxels_syn, wtp_syn, wtn_syn, wfp_syn, wfn_syn = fmri_power_calculations(syn_dataset_non_visual, syn_dataset_visual, syn_n, syn_n, visual_vs_non_visual_rejecting_voxels, k=k)
+    fdr_real_power, percent_rejecting_voxels_real, wtp_real, wtn_real, wfp_real, wfn_real = fmri_power_calculations(real_dataset_non_visual, real_dataset_visual, real_n, real_n, visual_vs_non_visual_rejecting_voxels, k=k)
 
     fdr_test_power_for_n[i] = fdr_real_power
     syn_fdr_test_power_for_n[i] = fdr_syn_power
 
-    tp_ratios_syn_for_n[i][:] = tp_ratios_syn[:]
-    tn_ratios_syn_for_n[i][:] = tn_ratios_syn[:]
-    fp_ratios_syn_for_n[i][:] = fp_ratios_syn[:]
-    fn_ratios_syn_for_n[i][:] = fn_ratios_syn[:]
+    percent_rejecting_voxels_syn_for_n[i][:] = percent_rejecting_voxels_syn
+    percent_rejecting_voxels_real_for_n[i][:] = percent_rejecting_voxels_real
 
-    tp_ratios_real_for_n[i][:] = tp_ratios_real[:]
-    tn_ratios_real_for_n[i][:] = tn_ratios_real[:]
-    fp_ratios_real_for_n[i][:] = fp_ratios_real[:]
-    fn_ratios_real_for_n[i][:] = fn_ratios_real[:]
+    wtp_syn_for_n[i][:] = wtp_syn[:]
+    wtn_syn_for_n[i][:] = wtn_syn[:]
+    wfp_syn_for_n[i][:] = wfp_syn[:]
+    wfn_syn_for_n[i][:] = wfn_syn[:]
 
-    print("PERCENT COMPLETE: {0:.2f}%\r".format(100 * float(i) / float(len(n))), end='')
+    wtp_real_for_n[i][:] = wtp_real[:]
+    wtn_real_for_n[i][:] = wtn_real[:]
+    wfp_real_for_n[i][:] = wfp_real[:]
+    wfn_real_for_n[i][:] = wfn_real[:]
+
+    print("PERCENT COMPLETE: {0:.2f}%\r".format(100 * float(i+1) / float(len(n))), end='')
 
 # Plot curve of n vs power
-axes[4].plot(n, fdr_test_power_for_n, label='FDR Test Real')
-axes[4].plot(n, syn_fdr_test_power_for_n, label='FDR Test Syn')
+axes[4].plot(n[:10], fdr_test_power_for_n[:10], label='FDR Test Real')
+axes[4].plot(n[:10], syn_fdr_test_power_for_n[:10], label='FDR Test Syn')
 axes[4].set_title('Sample Size vs Power')
 axes[4].set_xlabel('Sample Size')
 axes[4].set_ylabel('Power')
 axes[4].set_ylim([-0.1, 1.1])
 axes[4].legend(loc="upper right")
 
-# Plot curve of n vs rejection overlaps
-# True Positive
-sns.tsplot(data=tp_ratios_real_for_n.T, time=n, ci=[68, 95], color='blue', condition='REAL', ax=axes[5])
-sns.tsplot(data=tp_ratios_syn_for_n.T, time=n, ci=[68, 95], color='orange', condition='SYN', ax=axes[5])
-axes[5].set_title('Sample Size vs True Positives')
+# Plot curve of percent rejecting voxels
+sns.tsplot(data=percent_rejecting_voxels_real_for_n.T, time=n, ci=[68, 95], color='blue', condition='REAL', ax=axes[5])
+sns.tsplot(data=percent_rejecting_voxels_syn_for_n.T, time=n, ci=[68, 95], color='orange', condition='SYN', ax=axes[5])
+axes[5].set_title('Sample Size vs Percent Significant Voxels')
 axes[5].set_xlabel('Sample Size')
-axes[5].set_ylabel('TP / |ROI Reject|')
-axes[5].set_ylim([-0.1, 1.1])
+axes[5].set_ylabel('% Sig Voxels')
 axes[5].legend(loc="upper right")
 
-# True Negative
-sns.tsplot(data=tn_ratios_real_for_n.T, time=n, ci=[68, 95], color='blue', condition='REAL', ax=axes[6])
-sns.tsplot(data=tn_ratios_syn_for_n.T, time=n, ci=[68, 95], color='orange', condition='SYN', ax=axes[6])
-axes[6].set_title('Sample Size vs True Negatives')
+# Plot curve of n vs rejection overlaps
+# True Positive
+sns.tsplot(data=wtp_real_for_n.T, time=n, ci=[68, 95], color='blue', condition='REAL', ax=axes[6])
+sns.tsplot(data=wtp_syn_for_n.T, time=n, ci=[68, 95], color='orange', condition='SYN', ax=axes[6])
+axes[6].set_title('Sample Size vs Weighted True Positive')
 axes[6].set_xlabel('Sample Size')
-axes[6].set_ylabel('TN / |ROI Accept|')
-axes[6].set_ylim([-0.1, 1.1])
+axes[6].set_ylabel('W_TP')
 axes[6].legend(loc="upper right")
 
-# False Positive
-sns.tsplot(data=fp_ratios_real_for_n.T, time=n, ci=[68, 95], color='blue', condition='REAL', ax=axes[7])
-sns.tsplot(data=fp_ratios_syn_for_n.T, time=n, ci=[68, 95], color='orange', condition='SYN', ax=axes[7])
-axes[7].set_title('Sample Size vs False Positives')
+# True Negative
+sns.tsplot(data=wtn_real_for_n.T, time=n, ci=[68, 95], color='blue', condition='REAL', ax=axes[7])
+sns.tsplot(data=wtn_syn_for_n.T, time=n, ci=[68, 95], color='orange', condition='SYN', ax=axes[7])
+axes[7].set_title('Sample Size vs Weighted True Negatives')
 axes[7].set_xlabel('Sample Size')
-axes[7].set_ylabel('FP / |ROI Accept|')
-axes[7].set_ylim([-0.1, 1.1])
+axes[7].set_ylabel('W_TN')
 axes[7].legend(loc="upper right")
 
-# False Negative
-sns.tsplot(data=fn_ratios_real_for_n.T, time=n, ci=[68, 95], color='blue', condition='REAL', ax=axes[8])
-sns.tsplot(data=fn_ratios_syn_for_n.T, time=n, ci=[68, 95], color='orange', condition='SYN', ax=axes[8])
-axes[8].set_title('Sample Size vs False Negatives')
+# False Positive
+sns.tsplot(data=wfp_real_for_n.T, time=n, ci=[68, 95], color='blue', condition='REAL', ax=axes[8])
+sns.tsplot(data=wfp_syn_for_n.T, time=n, ci=[68, 95], color='orange', condition='SYN', ax=axes[8])
+axes[8].set_title('Sample Size vs Weighted False Positives')
 axes[8].set_xlabel('Sample Size')
-axes[8].set_ylabel('FN / |ROI Reject|')
-axes[8].set_ylim([-0.1, 1.1])
+axes[8].set_ylabel('W_FP')
 axes[8].legend(loc="upper right")
+
+# False Negative
+sns.tsplot(data=wfn_real_for_n.T, time=n, ci=[68, 95], color='blue', condition='REAL', ax=axes[9])
+sns.tsplot(data=wfn_syn_for_n.T, time=n, ci=[68, 95], color='orange', condition='SYN', ax=axes[9])
+axes[9].set_title('Sample Size vs Weighted False Negatives')
+axes[9].set_xlabel('Sample Size')
+axes[9].set_ylabel('W_FN')
+axes[9].legend(loc="upper right")
 
 # Save results
 figure.subplots_adjust(hspace=0.5)

utils/multiple_comparison.py
@@ -14,35 +14,46 @@ def rejecting_voxels(d1, d2, alpha=0.05):
 
     return fdr_correction(two_sample_t_test_p_vals_by_voxel, alpha=alpha)[0]
 
+def bootstrap_rejecting_voxels_mask(d1, d2, k=10**1, alpha=0.05):
+    fdr_reject_by_voxel_boot = []
+
+    for br in range(k):
+        d1_idx = np.random.randint(low=0, high=d1.shape[0], size=d1.shape[0])
+        d2_idx = np.random.randint(low=0, high=d2.shape[0], size=d2.shape[0])
+
+        d1_replicate = d1[d1_idx].squeeze()
+        d2_replicate = d2[d2_idx].squeeze()
+
+        fdr_reject_by_voxel = rejecting_voxels(d1_replicate, d2_replicate, alpha)
+        fdr_reject_by_voxel_boot.append(fdr_reject_by_voxel)
+
+    return np.mean(fdr_reject_by_voxel_boot, axis=0)
+
+
 def rejection_mask_overlap(rejections, mask_rejections):
     tp = 0
     tn = 0
     fp = 0
     fn = 0
-    total_roi_reject = 0
-    total_roi_accept = 0
 
     for i in range(rejections.shape[0]):
         for j in range(rejections.shape[1]):
             for k in range(rejections.shape[2]):
-                rejection = rejections[i][j][k]
-                mask_reject = mask_rejections[i][j][k]
+                reject = rejections[i][j][k]
+                accept = not reject
+                mask_reject_weight = mask_rejections[i][j][k]
+                mask_accept_weight = 1.0 - mask_reject_weight
 
-                tp += (rejection and mask_reject)
-                tn += (not rejection and not mask_reject)
-                fp += (rejection and not mask_reject)
-                fn += (not rejection and mask_reject)
-                total_roi_reject += mask_reject
-                total_roi_accept += (not mask_reject)
+                tp += (mask_reject_weight * reject)
+                tn += (mask_accept_weight * accept)
+                fp += (mask_accept_weight * reject)
+                fn += (mask_reject_weight * accept)
 
-    tp = float(tp) / float(max(total_roi_reject, 1))
-    tn = float(tn) / float(max(total_roi_accept, 1))
-    fp = float(fp) / float(max(total_roi_accept, 1))
-    fn = float(fn) / float(max(total_roi_reject, 1))
     return tp, tn, fp, fn
 
 def fmri_power_calculations(d1, d2, n_1, n_2, overlap_mask, alpha=0.05, k=10**1):
     fdr_rejections = []
+    percent_rejecting_voxels = []
     tp_ratios = []
     tn_ratios = []
     fp_ratios = []
@@ -56,8 +67,12 @@ def fmri_power_calculations(d1, d2, n_1, n_2, overlap_mask, alpha=0.05, k=10**1)
         d2_replicate = d2[d2_idx].squeeze()
 
         fdr_reject_by_voxel = rejecting_voxels(d1_replicate, d2_replicate, alpha)
-        fdr_reject = sum(fdr_reject_by_voxel) > 0  # reject if any voxel rejects
+        flattened_fdr_reject_by_voxel = fdr_reject_by_voxel.flatten()
+        fdr_reject = np.sum(flattened_fdr_reject_by_voxel) > 0  # reject if any voxel rejects
+        percent_reject = np.count_nonzero(flattened_fdr_reject_by_voxel) / flattened_fdr_reject_by_voxel.shape[0]
+
         fdr_rejections.append(fdr_reject)
+        percent_rejecting_voxels.append(percent_reject)
 
         tp, tn, fp, fn = rejection_mask_overlap(fdr_reject_by_voxel, overlap_mask)
         tp_ratios.append(tp)
@@ -66,7 +81,7 @@ def fmri_power_calculations(d1, d2, n_1, n_2, overlap_mask, alpha=0.05, k=10**1)
         fn_ratios.append(fn)
 
     fdr_power = np.mean(fdr_rejections)
-    return fdr_power, tp_ratios, tn_ratios, fp_ratios, fn_ratios
+    return fdr_power, percent_rejecting_voxels, tp_ratios, tn_ratios, fp_ratios, fn_ratios
 
 def multivariate_power_calculation(d1, d2, n_1, n_2, alpha=0.05, k=10**1):
     fdr_rejections = []
@@ -78,6 +93,7 @@ def multivariate_power_calculation(d1, d2, n_1, n_2, alpha=0.05, k=10**1):
         d1_replicate = d1[d1_idx].squeeze()
         d2_replicate = d2[d2_idx].squeeze()
 
+        # FDR corrected univariate tests
         two_sample_t_test_p_vals_by_dim = np.zeros(d1.shape[1:])
         for i in range(two_sample_t_test_p_vals_by_dim.shape[0]):
             d1_vals = d1[:, i]
@@ -89,7 +105,8 @@ def multivariate_power_calculation(d1, d2, n_1, n_2, alpha=0.05, k=10**1):
         fdr_rejections.append(fdr_reject)
 
     fdr_power = np.mean(fdr_rejections)
-    return fdr_power
+    # hotellings_power = 0.2#np.mean(hotellings_rejections)
+    return fdr_power#, hotellings_power
 
 # Authors: Josef Pktd and example from H Raja and rewrite from Vincent Davis
 #          Alexandre Gramfort <alexandre.gramfort@telecom-paristech.fr>