4d8b81b0658c8dfc9dbef410020df6f3cbd76730
BlissChapman <bliss.chapman@gmail.com>
2018-05-02 20:16:54 -0500

    Updated ProbabilityDistGAN to be able to capture multivariate distributions.

--- models/ProbabilityDistGAN.py
+++ models/ProbabilityDistGAN.py
@@ -13,9 +13,9 @@ class Generator(nn.Module):
         super(Generator, self).__init__()
 
         self.fc_1 = nn.Linear(input_width, dimensionality)
-        #self.fc_2 = nn.Linear(dimensionality, dimensionality)
-        #self.fc_3 = nn.Linear(dimensionality, dimensionality)
-        #self.fc_4 = nn.Linear(dimensionality, dimensionality)
+        self.fc_2 = nn.Linear(dimensionality, dimensionality)
+        self.fc_3 = nn.Linear(dimensionality, dimensionality)
+        self.fc_4 = nn.Linear(dimensionality, dimensionality)
         self.fc_5 = nn.Linear(dimensionality, output_width)
 
         self.optimizer = optim.Adam(self.parameters(), lr=1e-4, betas=(0.5, 0.9))
@@ -29,17 +29,17 @@ class Generator(nn.Module):
         out = F.leaky_relu(out, inplace=True)
         out = F.dropout(out, p=0.5)
 
-        # out = self.fc_2(out)
-        # out = F.leaky_relu(out, inplace=True)
-        # out = F.dropout(out, p=0.5)
-        #
-        # out = self.fc_3(out)
-        # out = F.leaky_relu(out, inplace=True)
-        # out = F.dropout(out, p=0.5)
-        #
-        # out = self.fc_4(out)
-        # out = F.leaky_relu(out, inplace=True)
-        # out = F.dropout(out, p=0.5)
+        out = self.fc_2(out)
+        out = F.leaky_relu(out, inplace=True)
+        out = F.dropout(out, p=0.5)
+
+        out = self.fc_3(out)
+        out = F.leaky_relu(out, inplace=True)
+        out = F.dropout(out, p=0.5)
+
+        out = self.fc_4(out)
+        out = F.leaky_relu(out, inplace=True)
+        out = F.dropout(out, p=0.5)
 
         out = self.fc_5(out)
         return out
@@ -60,9 +60,9 @@ class Critic(nn.Module):
         super(Critic, self).__init__()
 
         self.fc_1 = nn.Linear(input_width, dimensionality)
-        # self.fc_2 = nn.Linear(dimensionality, dimensionality)
-        # self.fc_3 = nn.Linear(dimensionality, dimensionality)
-        # self.fc_4 = nn.Linear(dimensionality, dimensionality)
+        self.fc_2 = nn.Linear(dimensionality, dimensionality)
+        self.fc_3 = nn.Linear(dimensionality, dimensionality)
+        self.fc_4 = nn.Linear(dimensionality, dimensionality)
         self.fc_5 = nn.Linear(dimensionality, 1)
 
         self.cudaEnabled = cudaEnabled
@@ -75,14 +75,14 @@ class Critic(nn.Module):
         out = self.fc_1(data)
         out = F.leaky_relu(out, inplace=True)
 
-        # out = self.fc_2(out)
-        # out = F.leaky_relu(out, inplace=True)
-        #
-        # out = self.fc_3(out)
-        # out = F.leaky_relu(out, inplace=True)
-        #
-        # out = self.fc_4(out)
-        # out = F.leaky_relu(out, inplace=True)
+        out = self.fc_2(out)
+        out = F.leaky_relu(out, inplace=True)
+
+        out = self.fc_3(out)
+        out = F.leaky_relu(out, inplace=True)
+
+        out = self.fc_4(out)
+        out = F.leaky_relu(out, inplace=True)
 
         out = self.fc_5(out)
         return out
